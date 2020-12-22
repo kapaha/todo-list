cd190f1c4c87c4caa2e293d0ec152daf2b0b0d33
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2020-12-22 13:41:31 +0000

    feat: Add app init() function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import addGlobalEventListener from './events';
 import { toggleSideMenu } from './dom';
 
-// Add event listeners
-addGlobalEventListener('click', '[data-header-toggle]', toggleSideMenu);
+init();
+
+function init() {
+    initGlobalEventListeners();
+}
+
+function initGlobalEventListeners() {
+    addGlobalEventListener('click', '[data-header-toggle]', toggleSideMenu);
+}
