54dc4c86b6fd5375d3f1ea005b8531eb6269565c
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2020-12-22 13:56:06 +0000

    refactor: Cleanup toggleSideMenu()

--- src/dom.js
+++ src/dom.js
@@ -1,24 +1,22 @@
+export function toggleSideMenu() {
+    if (elems.sideMenu == null) return;
+
+    if (isDesktopView()) {
+        elems.body.classList.toggle('desktop-open');
+        elems.sideMenu.classList.remove('mobile-open');
+    } else if (!elems.body.classList.contains('desktop-open')) {
+        elems.body.classList.add('desktop-open');
+        elems.sideMenu.classList.toggle('mobile-open');
+    } else {
+        elems.sideMenu.classList.toggle('mobile-open');
+    }
+}
+
 const elems = {
     body: document.body,
     sideMenu: document.querySelector('[data-side-menu]')
 };
 
-function toggleSideMenu() {
-    if (elems.sideMenu) {
-        if (isDesktopView()) {
-            elems.body.classList.toggle('desktop-open');
-            elems.sideMenu.classList.remove('mobile-open');
-        } else if (!elems.body.classList.contains('desktop-open')) {
-            elems.body.classList.add('desktop-open');
-            elems.sideMenu.classList.toggle('mobile-open');
-        } else {
-            elems.sideMenu.classList.toggle('mobile-open');
-        }
-    }
-}
-
 function isDesktopView() {
     return window.matchMedia('(min-width: 768px)').matches;
 }
-
-export { toggleSideMenu };
